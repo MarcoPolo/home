bb0abba77b7ae89be9bbad2ec2f3469915f17a52
Marco Munizaga <marco@marcopolo.io>
2020-04-29 15:42:02 -0700

    First commit

---
+++ home.nix
@@ -0,0 +1,51 @@
+{ config, pkgs, ... }:
+
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "20.03";
+
+  programs.git = {
+    enable = true;
+    userName = "Marco Munizaga";
+    userEmail = "git@marcopolo.io";
+    aliases = {
+      co = "checkout";
+      ci = "commit";
+      st = "status";
+      br = "branch";
+      hist = ''log --pretty=format:"%h %ad | %s%d [%an]" --graph --date=short'';
+      type = "cat-file -t";
+      dump = "cat-file -p";
+      lg =
+        "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+      lga =
+        "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --all";
+      lgd =
+        "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit -p";
+      df = "diff --color --color-words --abbrev";
+      d = "difftool";
+      up = "!git remote update -p; git merge --ff-only @{u}";
+    };
+
+    extraConfig = {
+      merge = { conflictstyle = "diff3"; };
+      push = { default = "current"; };
+      color = {
+        branch = "auto";
+        diff = "auto";
+        interactive = "auto";
+        status = "auto";
+      };
+    };
+  };
+}
