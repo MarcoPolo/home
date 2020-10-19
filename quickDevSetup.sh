848dd4b70179430ed501e3c445e7acd83123c1aa
Marco Munizaga <git@marcopolo.io>
2020-10-19 08:58:09 -0700

    Update quick dev setup

--- quickDevSetup.sh
+++ quickDevSetup.sh
@@ -2,7 +2,7 @@
 # Quick way to setup nix, home-manager, and marco's home manager config
 set -e
 curl -L https://nixos.org/nix/install | sh
-. /home/marco/.nix-profile/etc/profile.d/nix.sh
+. $HOME/.nix-profile/etc/profile.d/nix.sh
 nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 (cd ~/.config && git clone https://github.com/MarcoPolo/home nixpkgs)
